5b1188d38c3a9db2041bb012a5af27119d8612d2
Woojin Son <swj1265@naver.com>
2025-06-14 02:02:35 +0900

    🔧 #9 config(vitest) : add options to reload ts files when test is run

--- engines/typescript-obfuscator/vitest.config.ts
+++ engines/typescript-obfuscator/vitest.config.ts
@@ -3,17 +3,31 @@ import { defineConfig } from 'vitest/config';
 import path from 'path';
 
 export default defineConfig({
+    server: {
+        watch: {
+            ignored: ['**/dist/**', '**/node_modules/**'],
+        },
+
+    },
     test: {
         globals: true,
         environment: 'node',
         setupFiles: [path.resolve(__dirname, 'src/__test__/setup.ts')],
         silent: false,
-        reporters: 'default',
-        outputFile: undefined,
+        exclude: [
+            'src/__test__/fixtures/setup.ts',
+            'src/__test__/setup.ts',
+            'dist/**', 'node_modules/**'
+        ],
+        include: [
+            'src/__test__/**/*.ts'
+        ],
+
     },
     resolve: {
         alias: {
             '@declassify/shared-config': path.resolve(__dirname, '../../libs/shared-config/dist'),
-        }
+        },
+        extensions: ['.ts', '.mts', '.cts', '.js', '.mjs', '.cjs', '.json'],
     },
 });
